3c8ae266ce05a83ebe03254fda0827917ed3d1e7
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-03 02:17:56 +0000

    updated tests

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,9 +1,35 @@
-package example
+package com.madhukaraphatak.akkahttp.testable
 
-import org.scalatest._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.util.ByteString
+import org.scalatest.{Matchers, WordSpec}
+import akka.http.scaladsl.model._
 
-class HelloSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+
+/**
+  * Created by madhu on 8/11/15.
+  */
+class HelloSpec extends WordSpec with Matchers with ScalatestRouteTest with RestService{
+  "Customer API" should {
+    "Posting to /customer should add the customer" in {
+
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "name":"test"
+           |}
+        """.stripMargin)
+
+      val postRequest = HttpRequest(
+        HttpMethods.POST,
+        uri = "/customer",
+        entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+
+      postRequest ~> route ~> check {
+        status.isSuccess() shouldEqual true
+      }
+    }
+
   }
 }
