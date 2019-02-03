09e0b3e69f2d4e02810d3603071eb5b4e3a83783
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-03 02:10:07 +0000

    simplified main case server

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,15 +3,12 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import scala.io.StdIn
 
 object WebServer {
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+    implicit val actorSystem = ActorSystem("my-system")
+    implicit val actorMaterializer = ActorMaterializer()
 
     val route =
       path("hello") {
@@ -20,12 +17,6 @@ object WebServer {
         }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+    Http().bindAndHandle(route, "localhost", 8080)
   }
 }
