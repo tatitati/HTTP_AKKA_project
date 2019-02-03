dc214eb5f61f2936e91a99231c427e513af3efab
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-03 02:15:23 +0000

    removed things

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,6 +1,5 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
@@ -13,7 +12,9 @@ object WebServer {
     val route =
       path("hello") {
         get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+          complete(
+            "Say hello to akka-http"
+          )
         }
       }
 
