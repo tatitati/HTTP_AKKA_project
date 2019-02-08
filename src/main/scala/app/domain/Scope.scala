4afa74051361ff69686cecd72ef059798968440d
Francisco Albert Albusac <francisco.albusac@bbc.co.uk>
2019-02-08 13:02:06 +0000

    Update Scope.scala

--- src/main/scala/app/domain/Scope.scala
+++ src/main/scala/app/domain/Scope.scala
@@ -1,3 +1,3 @@
 package app.domain
 
-class Scope(val firstname: Boolean, val surname: Boolean, val email: Boolean)
+class Scope(val firstname: Boolean, val surname: Boolean, val email: Boolean) // Test edge case: what happens when someone creates an scope with everything forbidden????, it should throw an exception for sure
