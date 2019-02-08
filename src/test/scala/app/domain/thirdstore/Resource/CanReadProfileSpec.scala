139e2bce72d0e8cafd0314770baa6b68d3ec07e1
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-08 15:06:30 +0000

    simplified tests

--- src/test/scala/app/domain/thirdstore/Resource/CanReadProfileSpec.scala
+++ src/test/scala/app/domain/thirdstore/Resource/CanReadProfileSpec.scala
@@ -8,15 +8,21 @@ import scala.util.{Failure, Success}
 
 class CanReadProfileSpec extends FunSuite{
 
-  test("When token is live, then we check scope") {
+  test("Can read data when allowed and token is live") {
     val givenResource1 = BuildResource.withLiveToken(
       withsurname = "my surname",
       scope = Option(BuildScope.onlySurname())
     )
 
+    assert(givenResource1.surname() === Some("my surname"))
+  }
+
+  test("Cannot read data when not allowed even though token is live") {
+    val givenResource1 = BuildResource.withLiveToken(
+      scope = Option(BuildScope.onlySurname())
+    )
+
     assert(givenResource1.email() === None)
-    assert(givenResource1.firstname() === None)
-    assert(givenResource1.surname() === Some("my surname"), "=> Should return the surname because the scope allows it and the token is live")
   }
 
   test("Even if the scope allows it, if the token is expired then the profile cannot be read") {
@@ -24,9 +30,7 @@ class CanReadProfileSpec extends FunSuite{
       withsurname = "my surname",
       scope = Option(BuildScope.onlySurname())
     )
-
-    assert(givenResource2.email() === None)
-    assert(givenResource2.firstname() === None)
+    
     assert(givenResource2.surname() === None, "=> Should returns None because even when the scope allows it, the token is expired")
   }
 }
