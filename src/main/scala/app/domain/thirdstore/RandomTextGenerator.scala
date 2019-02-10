48a3c271e0c80f8664b5c0fec786fb2a965ac9ab
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-10 12:39:48 +0000

    more

--- src/main/scala/app/domain/thirdstore/RandomTextGenerator.scala
+++ src/main/scala/app/domain/thirdstore/RandomTextGenerator.scala
@@ -4,12 +4,6 @@ import scala.util.Random
 
 trait RandomTextGenerator {
   protected def generateRandomString(length: Int = 10): String = { // move this function to a trait?, an object?....
-    var text: String = ""
-
-    for(_ <- 1 to length) {
-      text = text + Random.nextPrintableChar
-    }
-
-    text
+    Random.alphanumeric take length mkString
   }
 }
