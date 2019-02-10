1051538b5636b28eac37bc6d44f299372acf45f2
Francisco Albert Albusac <francisco.albusac@bbc.co.uk>
2019-02-10 23:20:21 +0000

    Update Third.scala

--- src/main/scala/app/domain/thirdstore/Third.scala
+++ src/main/scala/app/domain/thirdstore/Third.scala
@@ -1,6 +1,6 @@
 package app.domain.thirdstore
 
-class Third(var profile: ThirdProfile, var resource: ResourceToken) extends RandomTextGenerator {
+class Third(var profile: ThirdProfile, var resource: ResourceToken) extends RandomTextGenerator { // the resource is not needed anymore here
 
   def refreshCredentials(): Unit = {
     profile.clientid = generateRandomString()
