46294d2f4a791e2500c9d65de343e954a79d3a7c
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-10 12:41:16 +0000

    updated third, now the string generator for credentials is separated in a trait whenre we can reuse it, for example, for code generation as well

--- src/main/scala/app/domain/thirdstore/Third.scala
+++ src/main/scala/app/domain/thirdstore/Third.scala
@@ -1,22 +1,9 @@
 package app.domain.thirdstore
 
-import scala.util.Random
-
-class Third(var profile: ThirdProfile, var resource: Resource) {
+class Third(var profile: ThirdProfile, var resource: Resource) extends RandomTextGenerator {
 
   def refreshCredentials(): Unit = {
     profile.clientid = generateRandomString()
     profile.clientsecret = generateRandomString()
-  }
-
-  private def generateRandomString(): String = { // move this function to a trait?, an object?....
-    val length = 10
-    var text: String = ""
-
-    for(_ <- 1 to length) {
-      text = text + Random.nextPrintableChar
-    }
-
-    text
   }
 }
