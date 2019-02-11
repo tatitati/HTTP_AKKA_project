fe7269eff55341ea9fd1499dee01f8deb8557ccd
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-11 21:26:54 +0000

    more

--- src/main/scala/app/domain/thirdstore/ResourceCode.scala
+++ src/main/scala/app/domain/thirdstore/ResourceCode.scala
@@ -1,7 +1,8 @@
 package app.domain.thirdstore
 
-import app.domain.Scope
+import app.domain.{Scope, Token}
 import app.domain.ownerstore.OwnerProfile
+import com.github.nscala_time.time.Imports.DateTime
 
 class ResourceCode(
                      val thirdProfile: ThirdProfile,
@@ -9,4 +10,18 @@ class ResourceCode(
                      val scope: Scope,
                      private val code: Code
                    ){
+
+  def toResourceToken(): ResourceToken = {
+    new ResourceToken(
+      thirdProfile = thirdProfile,
+      ownerProfile = ownerProfile,
+      scope = scope,
+      token = new Token(
+        accessToken = java.util.UUID.randomUUID,
+        refreshToken = java.util.UUID.randomUUID,
+        generatedIn = DateTime.now(),
+        tokenType = "bearer"
+      )
+    )
+  }
 }
--- src/test/scala/app/domain/thirdstore/ResourceCodeSpec.scala
+++ src/test/scala/app/domain/thirdstore/ResourceCodeSpec.scala
@@ -1,15 +1,15 @@
 package test.app.domain.thirdstore
 
-import app.domain.thirdstore.{ResourceToken, ResourceTokenFactory}
+import app.domain.thirdstore.ResourceToken
 import builders.BuildResourceCode
 import org.scalatest.FunSuite
 
-class ResourceTokenSpec extends FunSuite {
+class ResourceCodeSpec extends FunSuite {
 
   test("I can create a ResourceToken from a ResourceCode") {
     val resourceCode = BuildResourceCode.any()
 
-    val resourceToken = ResourceTokenFactory.fromResourceCode(resourceCode)
+    val resourceToken = resourceCode.toResourceToken()
 
     assert(resourceToken.isInstanceOf[ResourceToken])
     assert(resourceToken.isTokenExpired() === false)
