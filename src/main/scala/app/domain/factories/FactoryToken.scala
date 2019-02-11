d78d349e3bd75e5cd560ac77c6f4fdf375e99434
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-11 22:37:18 +0000

    added both factories: for code, and token

--- src/main/scala/app/domain/factories/FactoryToken.scala
+++ src/main/scala/app/domain/factories/FactoryToken.scala
@@ -1,5 +1,16 @@
 package app.domain.factories
 
+import app.domain.Token
+import com.github.nscala_time.time.Imports.DateTime
+
 object FactoryToken {
 
+  def createBearer(): Token = {
+      new Token(
+        accessToken = java.util.UUID.randomUUID,
+        tokenType = "bearer",
+        refreshToken = java.util.UUID.randomUUID,
+        generatedIn = DateTime.now()
+      )
+  }
 }
--- src/test/scala/app/domain/factories/FactoryTokenSpec.scala
+++ src/test/scala/app/domain/factories/FactoryTokenSpec.scala
@@ -1,7 +1,13 @@
 package app.domain.factories
 
+import app.domain.Token
 import org.scalatest.FunSuite
 
 class FactoryTokenSpec extends FunSuite {
 
+  test("Can create new Tokens") {
+    val token = FactoryToken.createBearer()
+
+    assert(token.isInstanceOf[Token])
+  }
 }
