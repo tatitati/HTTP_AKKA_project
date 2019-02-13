978f5edf8f5c1a6461e2a2b9677da3c53615995f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-13 16:58:55 +0000

    added specs for date

--- src/test/scala/learning/DateTimeSpec.scala
+++ src/test/scala/learning/DateTimeSpec.scala
@@ -5,37 +5,42 @@ import org.scalatest.FunSuite
 
 class DateTimeSpec extends FunSuite {
 
-    test("I can create a date") {
-      val date = DateTime.now()
-      assert(date.isInstanceOf[DateTime])
-    }
+  test("Create a date") {
+    val date = DateTime.now()
+    assert(date.isInstanceOf[DateTime])
+  }
 
-  test("I can create a date in the future and compare with NOW") {
+  test("Compare two dates") {
     val now = DateTime.now()
     val dateFuture = DateTime.now() + 2.months
 
     assert(now < dateFuture)
   }
 
-  test("I can create an specific date") {
-    val specificdate1 = new DateTime().withYear(2030).withMonth(8).withDay(20)
+  test("Create an specific date in multiple ways") {
+    val specificdate1 = new DateTime()
+      .withYear(2030)
+      .withMonth(8)
+      .withDay(20)
+
     val specificdate2 = new DateTime()
       .withDate(2030, 8, 20)
       .withTime(13, 8, 20, 400)
 
     assert(specificdate1.getYear === 2030)
-    assert(specificdate1.getMonthOfYear === 8)
-    assert(specificdate2.toString === "2030-08-20T13:08:20.400+01:00")
+    assert(specificdate2.getMonthOfYear === 8)
   }
 
-  test("I can format an string in an specific format") {
-    val now = new  DateTime()
-    val specificdate = now.withYear(2030).withMonth(8).withDay(20)
+  test("Format an string in an specific format") {
+    val specificdate = new DateTime()
+      .withDate(2030, 8, 20)
+      .withTime(13, 8, 20, 400)
 
+    assert(specificdate.toString === "2030-08-20T13:08:20.400+01:00")
     assert(specificdate.toString("Y-M--d") === "2030-8--20")
   }
 
-  test("I can create a DateTime from an string") {
+  test("Can create a DateTime from an string") {
     val givenDateText = "2030-08-20T13:08:20.400+01:00"
     val givenDateEquivalent = new DateTime()
       .withDate(2030, 8, 20)
