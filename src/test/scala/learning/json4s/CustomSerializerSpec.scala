dd0e32135ec02a9be794ff4ebab38369eabed2ff
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-17 13:57:05 +0000

    added how to query from parsed json

--- src/test/scala/learning/json4s/CustomSerializerSpec.scala
+++ src/test/scala/learning/json4s/CustomSerializerSpec.scala
@@ -1,47 +1,14 @@
 package learning.serialize
 
 import com.github.nscala_time.time.Imports._
+
 import org.json4s._
-import org.json4s.JsonDSL._
-import org.json4s.JsonAST
-import org.json4s.jackson.JsonMethods._
-import org.scalatest.FunSuite
-import org.json4s.native.Serialization
-import org.json4s.native.Serialization.{read, write}
+import org.json4s.native.Serialization.write
 import org.scalatest.FunSuite
 import FieldSerializer._
 
 class GivenClass(val firstName: String, val age: Int)
 class GivenClassWithDate(val firstName: String, val date: DateTime)
-//
-//class SerializerDateTime extends Serializer[DateTime] {
-//  private val Class = classOf[DateTime]
-//
-//  def serialize(implicit format: Formats) = {
-//    case variable: DateTime => JString(variable.toString("===YYYY==="))
-//  }
-//
-//  def deserialize(implicit format: Formats) = {
-//    case (TypeInfo(Class, _), json) => json match {
-//      case JField("date",JString(value)) =>
-//            DateTime.parse(value, DateTimeFormat.forPattern("===YYYY==="))
-//      case x => throw new MappingException("Can't convert " + x + " to DateTime")
-//    }
-//  }
-//}
-//
-
-//class SerializerDateTimeCustom extends CustomSerializer[Interval](format => (
-//  {
-//    case JObject(JField("date", JInt(s)) :: JField("end", JInt(e)) :: Nil) =>
-//      new Interval(s.longValue, e.longValue)
-//  },
-//  {
-//    case x: Interval =>
-//      JObject(JField("start", JInt(BigInt(x.startTime))) :: JField("end", JInt(BigInt(x.endTime))) :: Nil)
-//  }
-//))
-
 
 class CustomSerializerSpec extends FunSuite {
 
@@ -66,34 +33,4 @@ class CustomSerializerSpec extends FunSuite {
 
     assert(jsonString === """{"firstName":"francisco","date":{}}""")
   }
-
-  test("We can enforce a format for dates") {
-    implicit val formats = DefaultFormats
-
-    val instance = new GivenClassWithDate("francisco", DateTime.now())
-    val jsonString = write(instance)
-
-    assert(jsonString === """{"firstName":"francisco","date":{}}""")
-  }
-
-//
-//  implicit val formats =  Serialization.formats(NoTypeHints) + new SerializerDateTime
-//
-//  test("I can convert to json an object using lift-json") {
-//    val jsonString = write(new GivenClass("francisco", DateTime.now()))
-//
-//    assert(jsonString === """{"name":"francisco","date":"===2019==="}""")
-//  }
-//
-////  test("Can deserialize using the custom deserializer") {
-////
-////    val givenJson = """{"name":"francisco","date":"===2019==="}"""
-////
-////    val value = parse(givenJson)
-////
-////    assert(value === JObject(List(JField("name",JString("francisco")), JField("date",JString("===2019===")))))
-////
-////
-////    println(value.extract)
-////  }
 }
--- src/test/scala/learning/json4s/ParseJsonSpec.scala
+++ src/test/scala/learning/json4s/ParseJsonSpec.scala
@@ -4,7 +4,7 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods.{compact, parse, render}
 import org.scalatest.FunSuite
 import org.json4s.native.Serialization
-import org.json4s.native.Serialization.{read, write}
+import org.json4s.native.Serialization.read
 
 class ParseJsonSpec extends FunSuite {
   class GivenClass(val firstName: String, val age: Int)
@@ -16,16 +16,29 @@ class ParseJsonSpec extends FunSuite {
 
       val parsed = parse(jsonString)
 
-      assert(parsed === JObject(List(("firstName",JString("francisco")), ("age",JInt(34)))))
+      assert(parsed === JObject(List(
+        ("firstName",JString("francisco")),
+        ("age",JInt(34))
+      )))
   }
 
+  test("Extract convert the parsed json into the specified object") {
+    implicit val formats = Serialization.formats(NoTypeHints)
+    val jsonString = """{"firstName":"francisco","age":34}"""
+    val parsed = parse(jsonString)
+
+    val age = parsed \\ "age"
+
+    assert(age === JInt(34))
+    assert(age.values === 34)
+  }
 
   test("I can parse a custom class into json directly, but no controlling the keys used or the format") {
     implicit val formats = Serialization.formats(NoTypeHints)
 
     val givenExample = new GivenClass("francisco", 34)
 
-    val parsed = Serialization.read[GivenClass]("""{"firstName":"francisco","age":34}""")
+    val parsed = read[GivenClass]("""{"firstName":"francisco","age":34}""")
 
     assert(parsed.age === 34)
     assert(parsed.firstName === "francisco")
@@ -43,52 +56,4 @@ class ParseJsonSpec extends FunSuite {
     val step4 = compact(step3)
     assert(step4 === """{"first_name":"francisco","age":34}""")
   }
-
-//  test("Can parse a flat json") {
-//    val givenJson = """
-//                   {
-//                      "user":"whatever",
-//                      "refresh_token":5
-//                    }
-//                    """
-//
-//    val parsed = parse(givenJson)
-//
-//    assert(parsed ===
-//      JObject(List(("user",JString("whatever")), ("refresh_token",JInt(5))))
-//    )
-//
-//    assert(parsed.values ===
-//      Map("user" -> "whatever", "refresh_token" -> 5)
-//    )
-//  }
-
-//
-//  test("after parsing, I can query some data") {
-//    case class Person(firstname: String, age: Int)
-//
-//    json transformField {
-//      case ("first-name", x) => ("firstname", x)
-//    }
-//
-//    val value = for {
-//      JObject(child) <- json
-//      JField("refresh_token", JString(tokenValue))  <- child
-//    } yield child
-//
-//    println(value)
-//  }
-
-//  test("Another way") {
-//    val givenJson = """
-//                   {
-//                      "access_token":"whatever",
-//                      "refresh_token":5
-//                    }
-//                    """
-//
-//
-//
-//    assert(extracted === Some(Map("access_token" -> "whatever", "refresh_token" -> 5.0)))
-//  }
 }
