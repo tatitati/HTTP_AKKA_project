59582c867edb8e2687d3ef18d5629a901bc76863
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-17 14:23:07 +0000

    moe

--- src/test/scala/learning/DateTime/DateTimeCreationSpec.scala
+++ src/test/scala/learning/DateTime/DateTimeCreationSpec.scala
@@ -1,5 +1,6 @@
 package learning.DateTime
 
+
 import com.github.nscala_time.time.Imports._
 import org.scalatest.FunSuite
 
@@ -34,12 +35,13 @@ class DateTimeCreationSpec extends FunSuite {
   }
 
   test("Format an string in an specific format") {
+
     val specificdate = new DateTime()
       .withDate(2030, 8, 20)
       .withTime(13, 8, 20, 20)
-      .withZone(DateTimeZone.UTC)
 
-    assert(specificdate.toString === "2030-08-20T12:08:20.020Z")
+    println(specificdate)
+    assert(specificdate.toString === "2030-08-20T13:08:20.020+01:00")
     assert(specificdate.toString("Y-M--d") === "2030-8--20")
   }
 
