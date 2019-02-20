4f6cdaae2e0580b443988949ca5dc4826f41768b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-02-20 21:38:20 +0000

    improved studio about dates in json4s

--- src/test/scala/learning/json4s/ParseJsonSpec.scala
+++ src/test/scala/learning/json4s/ParseJsonSpec.scala
@@ -1,15 +1,14 @@
 package learning.serialize
 
-import org.joda.time.DateTime
+import org.scalatest.FunSuite
 import org.json4s._
 import org.json4s.jackson.JsonMethods.{compact, parse, render}
-import org.scalatest.FunSuite
 import org.json4s.native.Serialization
 import org.json4s.native.Serialization.read
 
 class ParseJsonSpec extends FunSuite {
   class GivenClass(val firstName: String, val age: Int)
-  class GivenClassWithDate(val firstName: String, val date: DateTime)
+
 
   test("Parse returns a JSobject") {
       implicit val formats = Serialization.formats(NoTypeHints)
@@ -29,6 +28,7 @@ class ParseJsonSpec extends FunSuite {
     val age = parsed \\ "age"
 
     assert(age === JInt(34))
+    assert(age.extract[Int] === 34)
     assert(age.values === 34)
   }
 
@@ -44,31 +44,6 @@ class ParseJsonSpec extends FunSuite {
     }
 
     assert(newparsed === JObject(List(("whatever",JString("FRANCISCO")), ("age",JInt(34)))))
-  }
-
-  test("Dates are parsed as string") {
-    val jsonString = """{"firstName":"francisco","date":"2030-02-20T13:08:20.020Z"}"""
-    val parsed = parse(jsonString)
-
-    val parsedDate = parsed \\ "date"
-
-    assert(parsedDate === JString("2030-02-20T13:08:20.020Z"))
-  }
-
-  test("Can transform dates strings to Date, but no directly") {
-    implicit val formats = DefaultFormats
-
-    val jsonString = """{"firstName":"francisco","date":"2030-02-20T13:08:20.020Z"}"""
-    val parsed = parse(jsonString)
-
-    val givenWithDate = new GivenClassWithDate(
-      firstName = (parsed \\ "firstName").extract[String],
-      date = new DateTime({
-        (parsed \\ "date").extract[String]
-      })
-    )
-
-    assert(givenWithDate.date.toString() === "2030-02-20T13:08:20.020Z")
   }
 
   test("I can parse a custom class into json directly, but no controlling the keys used or the format") {
