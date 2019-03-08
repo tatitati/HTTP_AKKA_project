45766c221e2e958464924e4e39c56eab5d5a89f5
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-03-08 20:15:06 +0000

    veeeeery clear the concept about rules in aggregates....

--- src/test/scala/learning/AggregatesSpec.scala
+++ src/test/scala/learning/AggregatesSpec.scala
@@ -4,21 +4,32 @@ import org.scalatest.FunSuite
 
 class AggregatesSpec extends FunSuite {
 
+  class Image(var title: String)
+  class IdCard(var photo: Image)
+
+
   test("Inner entities can be updated from outside, what SHOULDNT HAPPEN ever") {
-
-    class Image(var title: String = "title of my image")
-    class IdCard(var photo: Image)
-
     var myidcard = new IdCard(
-      new Image()
+      new Image("title of my image")
     )
 
-
-    assert(myidcard.photo.title == "title of my image")
+    assert(myidcard.photo.title === "title of my image")
 
     myidcard.photo.title = "new modified title from outside"
 
-    assert(myidcard.photo.title == "new modified title from outside")
+    assert(myidcard.photo.title === "new modified title from outside")
   }
 
+  test("Objects are passed by reference, so even using different variables, the aggregate is still updated from outside, THIS SHOULDNT HAPPEN EVER!") {
+    var myidcard = new IdCard(
+      new Image("title of my image")
+    )
+
+    assert(myidcard.photo.title === "title of my image")
+
+    var photo = myidcard.photo
+    photo.title = "new modified title from outside"
+
+    assert(myidcard.photo.title === "new modified title from outside")
+  }
 }
