2a33fb90d2aa8e921d33841dd3568cb492e9e6f5
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-03-13 00:58:28 +0000

    more formatting

--- src/main/scala/app/infrastructure/repository/third/ThirdPersistedModel.scala
+++ src/main/scala/app/infrastructure/repository/third/ThirdPersistedModel.scala
@@ -1,11 +1,11 @@
 package app.infrastructure.repository.third
 
 case class ThirdPersistedModel(
-                                id: Option[Long] = None,
-                                name: String,
-                                callback: String,
-                                homepage: String,
-                                description: String,
-                                clientId: String,
-                                clientSecret: String
-                              )
+  id: Option[Long] = None,
+  name: String,
+  callback: String,
+  homepage: String,
+  description: String,
+  clientId: String,
+  clientSecret: String
+)
