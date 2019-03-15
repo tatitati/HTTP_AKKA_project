6a06208274e9b34770f44444089b15092fc40e1e
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-03-15 00:10:04 +0000

    added some basic persistence classes to make possible to store owners

--- src/main/scala/app/infrastructure/Persistence/owner/OwnerProfilePersistModel.scala
+++ src/main/scala/app/infrastructure/Persistence/owner/OwnerProfilePersistModel.scala
@@ -1,5 +1,12 @@
 package app.infrastructure.Persistence.owner
 
-class OwnerProfilePersistModel {
+import com.github.nscala_time.time.Imports.DateTime
 
-}
+case class OwnerProfilePersistModel (
+  id: Option[Long] = None,
+  firstname: String,
+  surname: String,
+  email: String,
+  isEmailConfirmed: Boolean,
+  datebirth: DateTime
+)
