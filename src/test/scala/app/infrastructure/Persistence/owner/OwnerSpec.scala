b5bdec86fbaef701324cf52d808f8deb94e154b4
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-03-19 23:11:26 +0000

    more

--- src/test/scala/app/infrastructure/Persistence/owner/OwnerSpec.scala
+++ src/test/scala/app/infrastructure/Persistence/owner/OwnerSpec.scala
@@ -3,12 +3,18 @@ package app.infrastructure.Persistence.owner
 import app.infrastructure.Persistence.Exec
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
 import slick.jdbc.MySQLProfile.api._
+import slick.jdbc.meta.MTable
 import slick.lifted.TableQuery
 import test.app.infrastructure.Persistence.owner.OwnerProfileSchema
 
 class OwnerSpec extends FunSuite with BeforeAndAfterEach with Exec {
   val ownerSchema = TableQuery[OwnerProfileSchema]
   implicit val db = Database.forConfig("mydb")
+
+  test("third table exists") {
+    val tables = exec(MTable.getTables).toList
+    assert(tables.exists(_.name.name == "third") === true)
+  }
 
   test("database forconfig type is:") {
     assert(db.isInstanceOf[Database])
@@ -18,8 +24,4 @@ class OwnerSpec extends FunSuite with BeforeAndAfterEach with Exec {
     exec(ownerSchema.schema.dropIfExists)
     exec(ownerSchema.schema.create)
   }
-
-//  override def afterEach() {
-//    exec(ownerSchema.schema.drop)
-//  }
 }
