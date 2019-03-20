9e6ae5d3e6b2c5ce1e7bf3c0f6d552259638b730
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-03-20 23:45:17 +0000

    clean

--- src/test/scala/app/infrastructure/Persistence/resource/TokenMapperToPersistedSpec.scala
+++ src/test/scala/app/infrastructure/Persistence/resource/TokenMapperToPersistedSpec.scala
@@ -5,23 +5,23 @@ import org.scalatest.FunSuite
 
 class TokenMapperToPersistedSpec extends FunSuite {
     test("Domain token can be mapped to persisted") {
-      var givenDomainToken = BuildToken.anyLive()
-      var persistedModel = TokenMapper.toPersistence(givenDomainToken)
+      val givenDomainToken = BuildToken.anyLive()
+      val persistedModel = TokenMapper.toPersistence(givenDomainToken)
 
       assert(persistedModel.isInstanceOf[TokenPersistentModel])
       assert(persistedModel.tokenType === "bearer")
     }
 
     test("Surrogate id is also mapped to persisted domain") {
-      var givenDomainToken = BuildToken.anyLive(withSurrogateId = Some(12))
-      var persistedModel = TokenMapper.toPersistence(givenDomainToken)
+      val givenDomainToken = BuildToken.anyLive(withSurrogateId = Some(12))
+      val persistedModel = TokenMapper.toPersistence(givenDomainToken)
 
       assert(persistedModel.id === Some(12))
     }
 
   test("Surrogate id is also mapped when is None") {
-    var givenDomainToken = BuildToken.anyLive(withSurrogateId = None)
-    var persistedModel = TokenMapper.toPersistence(givenDomainToken)
+    val givenDomainToken = BuildToken.anyLive(withSurrogateId = None)
+    val persistedModel = TokenMapper.toPersistence(givenDomainToken)
 
     assert(persistedModel.id === None)
   }
