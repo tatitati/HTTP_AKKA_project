3f5063868d0e5b90636e7d2ece4a490247498b9f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-03-26 23:47:00 +0000

    more

--- src/test/scala/app/domain/model/user/BuildUser.scala
+++ src/test/scala/app/domain/model/user/BuildUser.scala
@@ -1,16 +1,22 @@
 package test.app.domain.user
 
 import app.domain.user.User
-import app.domain.model.auth.AuthId
 import app.domain.model.user.{UserId, UserProfile}
-import test.app.domain.model.auth.BuildAuthId
 import test.app.domain.model.user.BuildUserId
+import test.builders.BuildSurrogateId
 
 object BuildUser {
     def any(
+             withSurrogateId: Option[Long] =  BuildSurrogateId.any(),
              withId: UserId = BuildUserId.any(),
              withProfile: UserProfile = BuildUserProfile.any()
            ):User  = {
-      new User(withId, withProfile)
+      val user = new User(withId, withProfile)
+
+      if(withSurrogateId != None) {
+        user.setSurrogateId(withSurrogateId)
+      }
+
+      user
     }
 }
--- src/test/scala/app/domain/model/user/BuildUserProfile.scala
+++ src/test/scala/app/domain/model/user/BuildUserProfile.scala
@@ -1,14 +1,12 @@
 package test.app.domain.user
 
 import app.domain.model.user.UserProfile
-import test.builders.BuildSurrogateId
 import com.github.nscala_time.time.Imports.DateTime
 import test.builders.Faker
 
 object BuildUserProfile {
 
   def any(
-           withSurrogateId: Option[Long] =  BuildSurrogateId.any(),
            withFirstname: String =  Faker.text(),
            withSurname: String = Faker.text(),
            withEmail: String = Faker.text(),
